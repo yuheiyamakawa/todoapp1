6de6e14b313f0a9d3283e7c318cd5c0ba99153b1
Yuhei Yamakawa <ymkwyh1@gmail.com>
2021-06-19 15:14:20 +0900

    profileを作成（avatar, nickname, introduction） headerにプロフィール画像を表示 boardに作成者のプロフィール画像を表示

---
+++ app/controllers/profiles_controller.rb
@@ -0,0 +1,29 @@
+class ProfilesController < ApplicationController
+    before_action :authenticate_user!
+
+    def show
+        @profile = current_user.profile
+    end
+
+    def edit
+        @profile = current_user.prepare_profile
+    end
+
+    def update
+        @profile = current_user.prepare_profile
+        @profile.assign_attributes(profile_params)
+        if @profile.save
+          redirect_to profile_path, notice: '更新できました'
+        else
+          flash.now[:error] = '更新できませんでした'
+          render :edit
+        end
+    end
+
+    private
+    def profile_params
+        params.require(:profile).permit(:nickname, :introduction, :avatar)
+    end
+
+end
+  
